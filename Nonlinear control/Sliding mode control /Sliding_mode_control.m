8e532493e71f19e9ea56027e0fa7f30c943ea06b
bomlee <simonkro@gmail.com>
2017-09-29 08:45:26 +0200

    data

--- Nonlinear control/Sliding mode control /Sliding_mode_control.m
+++ Nonlinear control/Sliding mode control /Sliding_mode_control.m
@@ -3,13 +3,13 @@ clc; clear all; close all;
 % Topic: Sliding mode control.
 
 %% Variables and bounderies
-global l m k g gain dummy int_x1 int_x2
+global l m k g gain dummy int_x1 int_x2 
 l = rand(1)*(1.1 - 0.9) + 0.9; % Random pick lenght with boundery 0.9<= l <= 1.1
 m = rand(1)*(1.5 - 0.5) + 0.5; % Random pick mass with boundery 0.5 <= m <= 1.5
 k = rand(1)*(0.2 - 0);          % Random pick resistance with boundery 0 <= k <= 0.2
 g = 9.81;                     % Gravity
 gain = -10;
-t = [0:1:500]';
+t = [0:1:100]';
 dummy = 1;
               % Horizontal acceleration with random pick 
 int_x1 = 0.5;
@@ -29,25 +29,19 @@ xlabel('time')
 %px = (0.01/l) - ((0.2*0.01)/m) - g*sin(0.01) - gain*0.01
 
 function dx=xdot(t,x)
-global l m k g gain dummy int_x1 int_x2
+global l m k g gain dummy int_x1 int_x2 u
     
     if dummy == 1
-    u = (-16.1865*norm(int_x1) + 1.815*norm(int_x2) + 2)%*sat((int_x2-gain*int_x2)/0.004)
+    u = (-16.1865*norm(int_x1) + 1.815*norm(int_x2) + 2);%*sat((int_x2-gain*int_x2)/0.004)
     dummy = 0;
     end
     
     dx(1,1) = x(2);
-    dx(2,1) = (1/(m*l))*(m*(sin(t*2*pi))*cos(x(1)) - k*l*x(2) - m*g*l*sin(x(1))) + (1/(m*l^2));   
+    dx(2,1) = (1/(m*l))*(m*(sin(t*2*pi))*cos(x(1)) - k*l*x(2) - m*g*l*sin(x(1))) + (1/(m*l^2)*u);   
 
-if sign((x(2)-gain*x(2))/0.004) == 1
-    u = -1*(-16.1865*norm(x(1)) + 1.815*abs(x(2)) + 2)
-    'hi'
-end
+    u = -1*(-16.1865*norm(x(1)) + 1.815*norm(x(2)) + 2) * sign((x(2)-gain*x(2))/0.004)
+    
 
-if sign((x(2)-gain*x(2))/0.004) == 0
-    u = (-16.1865*norm(x(1)) + 1.815*norm(x(2)) + 2)
-    'ih'
-end
 
 end
 
